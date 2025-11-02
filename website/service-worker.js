4a87562e83f6c644d15e200f83eca408c7198644
Former-Stranger <akalbfell@gmail.com>
2025-11-02 17:51:59 -0500

    Bump service worker to v26

--- website/service-worker.js
+++ website/service-worker.js
@@ -1,8 +1,8 @@
 // Service Worker for Earplugs & Memories PWA
-// Version: 1.0.25
+// Version: 1.0.26
 
-const CACHE_NAME = 'earplugs-memories-v25';
-const DATA_CACHE_NAME = 'earplugs-memories-data-v25';
+const CACHE_NAME = 'earplugs-memories-v26';
+const DATA_CACHE_NAME = 'earplugs-memories-data-v26';
 
 // Files to cache immediately on install
 const STATIC_CACHE_URLS = [
